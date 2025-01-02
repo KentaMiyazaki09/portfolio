cd6cb9e1e59a6019f9faef34f0dd7a544d9857f1
KentaMiyazaki09 <kmiyazaki0924@gmail.com>
2025-01-02 23:38:53 +0900

    フリックで回転するオブジェクトを追加

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,22 +1,56 @@
 import * as THREE from 'three'
-// import mv from "./_modules/_mv"
 // import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import * as lil from 'lil-gui'
+
+/**
+ * UIデバッグ
+ */
+const gui = new lil.GUI()
+
+/**
+ * three
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
 
 let scene, canvas, camera, renderer
+
+/**
+ * ライト
+ */
 const addLight = () => {
-  // const directionalLight = new THREE.DirectionalLight(0xFFFFFF);
-  // directionalLight.position.set(1, 1, 1);
-  // scene.add(directionalLight)
+  const directionalLight = new THREE.DirectionalLight(0xffffff)
+  directionalLight.position.set(0.5, 1, 0)
+  scene.add(directionalLight)
 
-  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
-  scene.add(ambientLight)
-}
-const addCamera = ({width, height}) => {
-  camera = new THREE.PerspectiveCamera(45, width / height, 1, 100)
-  camera.position.set(0, 0, 10)
-  camera.rotation.set(0, 0, 0)
+  // const lightHelper = new THREE.DirectionalLightHelper(directionalLight, 5)
+  // scene.add(lightHelper)
+
+  const folderLight = gui.addFolder("Light")
+  folderLight.open()
+  folderLight.add(directionalLight.position, 'x', -1, 1, 0.01)
+  folderLight.add(directionalLight.position, 'y', -1, 1, 0.01)
+  folderLight.add(directionalLight.position, 'z', -1, 1, 0.01)
 }
 
+/**
+ * カメラ
+ */
+const addCamera = () => {
+  camera = new THREE.PerspectiveCamera(
+    35,
+    sizes.width / sizes.height,
+    0.1,
+    100
+  )
+  camera.position.set(0, 0, 12)
+}
+
+/**
+ * オブジェクト
+ */
 const createBox = () => {
   const geometry = new THREE.BoxGeometry(10, 8, 0.5)
   const loader = new THREE.TextureLoader()
@@ -37,10 +71,112 @@ const createBox = () => {
   scene.add(box)
 }
 
+let meshes = []
+const createMesh = () => {
+  const material = new THREE.MeshPhysicalMaterial({
+    color: '#3c94d7',
+    metalness: 0.86,
+    roughness: 0.37,
+    flatShading: true,
+  })
+
+  gui.addColor(material, "color")
+  gui.add(material, 'metalness', 0, 1, 0.01)
+  gui.add(material, 'roughness', 0, 1, 0.01)
+
+  const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 16, 60), material)
+  const mesh2 = new THREE.Mesh(new THREE.OctahedronGeometry(), material)
+  const mesh3 = new THREE.Mesh(new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16), material)
+  const mesh4 = new THREE.Mesh(new THREE.IcosahedronGeometry(), material)
+
+  mesh1.position.set(2, 0, 0)
+  mesh2.position.set(-1, 0, 0)
+  mesh3.position.set(2, 0, -6)
+  mesh4.position.set(5, 0, 3)
+
+  scene.add(mesh1, mesh2, mesh3, mesh4)
+  meshes.push(mesh1, mesh2, mesh3, mesh4)
+
+  // パーティクル
+  const particlesGeometry = new THREE.BufferGeometry()
+  const particlesCount = 700
+  const positionArray = new Float32Array(particlesCount * 3)
+  for(let i = 0; i < particlesCount * 3; i++) {
+    positionArray[i] = (Math.random() - 0.5) * 10
+  }
+
+  particlesGeometry.setAttribute(
+    'position',
+    new THREE.BufferAttribute(positionArray, 3)
+  )
+
+  const particlesMaterial = new THREE.PointsMaterial({
+    size: 0.025,
+    color: '#ffffff'
+  })
+  const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+  scene.add(particles)
+}
+
+function rot() {
+  rotation += speed
+  speed *= 0.93
+
+  meshes[0].position.x = 2 + 3.8 * Math.cos(rotation)
+  meshes[0].position.z = -3 + 3.8 * Math.sin(rotation)
+
+  meshes[1].position.x = 2 + 3.8 * Math.cos(rotation + Math.PI / 2)
+  meshes[1].position.z = -3 + 3.8 * Math.sin(rotation + Math.PI / 2)
+
+  meshes[2].position.x = 2 + 3.8 * Math.cos(rotation + Math.PI)
+  meshes[2].position.z = -3 + 3.8 * Math.sin(rotation + Math.PI)
+
+  meshes[3].position.x = 2 + 3.8 * Math.cos(rotation + 3* (Math.PI / 2))
+  meshes[3].position.z = -3 + 3.8 * Math.sin(rotation + 3* (Math.PI / 2))
+
+  window.requestAnimationFrame(rot)
+}
+
+// マウスフリックイベント
+let speed = 0
+let rotation = 0
+
+let startPoint = 0
+let endPoint = 0
+window.addEventListener('mousedown', (event) => {
+  startPoint = event.clientX
+})
+window.addEventListener('touchstart', (event) => {
+  startPoint = event.touches[0].pageX
+})
+function handleSpeed(start, end) {
+  speed += (start - end) * 0.0008
+}
+window.addEventListener('mouseup', (event) => {
+  endPoint = event.clientX
+  handleSpeed(startPoint, endPoint)
+})
+window.addEventListener('touchend', (event) => {
+  endPoint = event.changedTouches[0].pageX
+  handleSpeed(startPoint, endPoint)
+})
+
+// 描画
+const clock = new THREE.Clock()
 const render = () => {
   renderer.render(scene, camera)
-  // window.requestAnimationFrame(render)
+
+  // 回転
+  let getDeltaTime = clock.getDelta()
+  for(const mesh of meshes) {
+    mesh.rotation.x += 0.1 * getDeltaTime
+    mesh.rotation.y += 0.12 * getDeltaTime
+  }
+
+  window.requestAnimationFrame(render)
 }
+
+// 初期動作まとめ
 const init = () => {
   scene = new THREE.Scene()
   canvas = document.querySelector('#bg-canvas')
@@ -54,18 +190,34 @@ const init = () => {
   renderer.setSize(window.innerWidth, window.innerHeight)
 
   // light & camera
-  addCamera(canvas)
+  addCamera()
   addLight()
 
   // new OrbitControls(camera, canvas)
 
   // オブジェクトの追加
-  createBox()
+  // createBox()
+  createMesh()
 
   render()
+
+
+  rot()
 }
 
+// ロード
 window.onload = () => {
   init()
-  // mv()
+}
+
+// リサイズ
+window.onresize = () => {
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(window.devicePixelRatio)
 }
